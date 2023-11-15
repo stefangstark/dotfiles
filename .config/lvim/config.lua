5177fb79806d8cae589f6ec9b3d0ecec1669c1cc
Stefan G Stark <starks@inf.ethz.ch>
2023-11-15 17:42:28 -0500

    Update lvim

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -6,10 +6,11 @@
 lvim.keys.normal_mode["<Tab>"] = ":bnext<cr>"
 lvim.keys.normal_mode["<S-Tab>"] = ":bprev<cr>"
 
-lvim.colorscheme = 'onedark'
+lvim.colorscheme = 'onenord'
 
 -- install plugins
 lvim.plugins = {
+  "rmehri01/onenord.nvim",
   {
     'navarasu/onedark.nvim',
     config = function()
@@ -29,14 +30,15 @@ local pyright_opts = {
   settings = {
     pyright = {
       disableLanguageServices = false,
-      disableOrganizeImports = false
+      disableOrganizeImports = false,
     },
     python = {
+      -- reportGeneralTypeIssues = false,
       analysis = {
         autoImportCompletions = true,
         autoSearchPaths = true,
         diagnosticMode = "workspace", -- openFilesOnly, workspace
-        typeCheckingMode = "off", -- off, basic, strict
+        typeCheckingMode = "off",     -- off, basic, strict
         useLibraryCodeForTypes = true
       }
     }
@@ -45,15 +47,21 @@ local pyright_opts = {
 
 require("lvim.lsp.manager").setup("pyright", pyright_opts)
 
+-- lvim.builtin.treesitter.ensure_installed = {
+--   "python",
+-- }
+
 -- setup linting
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup { { command = "ruff", filetypes = { "python" } } }
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup { { command = "flake8", args = { "--max-line-length=89" }, filetypes = { "python" } } }
 
 -- setup formatting
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup { { name = "black" }, }
--- lvim.format_on_save.enabled = false
--- lvim.format_on_save.pattern = { "*.py" }
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup { { name = "black" },  { name = 'isort' } }
+lvim.format_on_save.enabled = true
+lvim.format_on_save.pattern = { "*.py" }
+
+
 
 require('swenv').setup({
   post_set_venv = function()
@@ -66,4 +74,3 @@ lvim.builtin.which_key.mappings["C"] = {
   name = "Python",
   c = { "<cmd>lua require('swenv.api').pick_venv()<cr>", "Choose Env" },
 }
-
