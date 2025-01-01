602d9b390397290b4d43361579c377d61102a203
Stefan Stark <stefan.g.stark@gmail.com>
2025-01-02 00:37:15 +0300

    Add sketchybar mode hinting

---
+++ sketchybar/.config/sketchybar/plugins/aerospace_mode.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+source ${CONFIG_DIR}/colors/catppuccin-frappe.sh
+
+case "$AEROSPACE_MODE" in
+main | "")
+  sketchybar --set $NAME label.drawing=off background.drawing=off
+  ;;
+
+disabled)
+  sketchybar --set $NAME \
+    label="disabled" \
+    label.drawing=on \
+    label.color=0xff"${COLOR_SURFACE0}" \
+    background.drawing=on \
+    background.color=0xff"${COLOR_RED}"
+  ;;
+
+launch)
+  sketchybar --set $NAME \
+    label=${AEROSPACE_MODE} \
+    label.drawing=on \
+    label.color=0xff"${COLOR_SURFACE0}" \
+    background.drawing=on \
+    background.color=0xff"${COLOR_SKY}"
+  ;;
+
+*)
+  sketchybar --set $NAME \
+    label=${AEROSPACE_MODE} \
+    label.drawing=on \
+    label.color=0xff"${COLOR_SURFACE0}" \
+    background.drawing=on \
+    background.color=0xff"${COLOR_ROSEWATER}"
+  ;;
+
+esac
--- sketchybar/.config/sketchybar/plugins/clock.sh
+++ sketchybar/.config/sketchybar/plugins/clock.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sketchybar --set "$NAME" label="$(date '+%d/%m %H:%M')"
+sketchybar --set "$NAME" label="$(date '+%a %b %m %H:%M')"
