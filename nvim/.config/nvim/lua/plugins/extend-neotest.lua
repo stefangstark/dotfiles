b8f2ba81d5be9eceaf0bb1255a5451cea6ad3a0f
Stefan Stark <stefan.g.stark@gmail.com>
2025-01-10 12:55:26 +0300

    [nvim] add test extra

---
+++ nvim/.config/nvim/lua/plugins/extend-neotest.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/neotest-python",
+  },
+  opts = {
+    adapters = {
+      ["neotest-python"] = {
+        -- Here you can specify the settings for the adapter, i.e.
+        runner = "pytest",
+        python = ".venv/bin/python",
+      },
+    },
+  },
+}
