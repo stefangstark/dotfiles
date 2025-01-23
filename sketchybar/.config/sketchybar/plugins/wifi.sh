a0d268fc80b69eb4b5abaea67a38036dc5d5ff5f
Stefan Stark <stefan.g.stark@gmail.com>
2025-01-23 11:34:08 +0100

    [skb] fix wifi color and padding

--- sketchybar/.config/sketchybar/plugins/wifi.sh
+++ sketchybar/.config/sketchybar/plugins/wifi.sh
@@ -18,20 +18,23 @@ IP_ADDRESS=$(scutil --nwi | grep address | sed 's/.*://' | tr -d ' ' | head -1)
 IS_VPN=$(scutil --nwi | grep -m1 'utun' | awk '{ print $1 }')
 
 if [[ $IS_VPN != "" ]]; then
-  COLOR="0xff${COLOR_TEAL}"
-  ICON=
-  LABEL="VPN"
-elif [[ $IP_ADDRESS != "" ]]; then
-  COLOR=${DEFAULT_TEXT_COLOR}
-  ICON=
-  LABEL=""
-else
-  COLOR="0xff${COLOR_RED}"
-  ICON=
-  LABEL="Not Connected"
-fi
+  sketchybar --set $NAME \
+    icon= \
+    label=VPN \
+    icon.drawing=on label.drawing=on \
+    label.color="0xff${COLOR_TEAL}" \
+    icon.color="0xff${COLOR_TEAL}"
 
-sketchybar --set $NAME \
-  icon=$ICON \
-  icon.font="Hack Nerd Font:Bold:16.0" \
-  label="$LABEL"
+elif [[ $IP_ADDRESS != "" ]]; then
+  sketchybar --set $NAME \
+    icon= \
+    icon.drawing=on label.drawing=off \
+    icon.color=${DEFAULT_TEXT_COLOR}
+
+else
+  sketchybar --set $NAME \
+    icon= \
+    icon.drawing=on label.drawing=off \
+    icon.color="0xff${COLOR_RED}"
+
+fi
